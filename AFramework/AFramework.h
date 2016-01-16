139e636d33d3ec72aa0c3366253ed19ddacf4441
Glen Hinkle <glen@empireenterprises.com>
2016-01-15 18:21:51 -0700

    Initial Commit

---
+++ AFramework/AFramework.h
@@ -0,0 +1,19 @@
+//
+//  AFramework.h
+//  AFramework
+//
+//  Created by Glen Hinkle on 1/15/16.
+//  Copyright © 2016 Zombie Dolphin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AFramework.
+FOUNDATION_EXPORT double AFrameworkVersionNumber;
+
+//! Project version string for AFramework.
+FOUNDATION_EXPORT const unsigned char AFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFramework/PublicHeader.h>
+
+
